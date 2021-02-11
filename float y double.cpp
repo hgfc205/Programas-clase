54d17876941fee146a302db020026043d4080def
Hektor Germain Flores Cabrera <72997155+hgfc205@users.noreply.github.com>
2021-02-11 15:58:32 -0700

    Create float y double.cpp

---
+++ float y double.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float f=22.594;
+	double d=22.648358743358343;
+	
+	cout<<"flotante: "<<f<<"  soprta hasta 3 numeros despues del punto decimal"<<endl;
+	cout<<"doble: "<<d<<"    soprta hasta 4 numeros despues del punto decimal";
+	return 0;
+}
